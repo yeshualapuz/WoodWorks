0040b1399ae37b6146d0e12020afcbd8154e9403
John Lopez <lopez.johnpatrick@ncst.edu.ph>
2025-08-24 10:47:32 +0800

    forgot file fixed

---
+++ JS/forgot2.js
@@ -0,0 +1,9 @@
+function resetPassword() {
+  const email = document.getElementById('email').value;
+  if (!email) {
+    alert("Please enter your email.");
+    return;
+  }
+  alert("Password reset link sent to " + email);
+  window.location.href = "index.html";
+}
